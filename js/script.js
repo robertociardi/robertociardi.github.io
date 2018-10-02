61ce48b0ff9759dbc6429183d90bf96561dbfd66
robertociardi <robyciardi93@gmail.com>
2018-10-02 18:24:35 +0200

    cookie policy added

--- js/script.js
+++ js/script.js
@@ -1,39 +1,7 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+$(document).ready(function() {
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("return-to-top").style.display = "block";
-    } else {
-        document.getElementById("return-to-top").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-// Satellite map
-function satellite_map() {
-    var var_location = new google.maps.LatLng(48.856847, 2.296832);
-
-    var var_mapoptions = {
-        center: var_location,
-        zoom: 16,
-        mapTypeId: 'satellite'
-    };
-
-    var var_map = new google.maps.Map(document.getElementById("map-container-3"),
-        var_mapoptions);
-
-    var var_marker = new google.maps.Marker({
-        position: var_location,
-        map: var_map,
-        title: "Paris, France"
+    /* cookie button */
+    $('#cookie-toggler').click(function(){
+       $('#cookies').hide();
     });
-}
-
-// Initialize maps
-google.maps.event.addDomListener(window, 'load', satellite_map);
+});
